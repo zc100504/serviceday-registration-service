9e49ee83750e660a3a33cc59d1d8440641263388
MrQin877 <kuanchinzhong@gmail.com>
2026-04-26 02:13:47 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'serviceday-local-dev-secret-key-2026')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('REGISTRATION_SERVICE_URL', '127.0.0.1 localhost').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
